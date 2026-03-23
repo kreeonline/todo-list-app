0460394061389eded56945de1d5b4d8fef8b9e4c
Sukree Supajitsawat <sukree.s@aware.co.th>
2026-03-23 12:33:43 +0700

    Init project configuration and UIs

---
+++ src/lib/todo.ts
@@ -0,0 +1,73 @@
+import z from 'zod';
+import type { Todo } from '@/features/todo-list/todo.type';
+
+export const titleMinLength = 3;
+
+export const addTodoFormSchema = z.object({
+  title: z.string({ error: 'Title must be string.' }).min(3, {
+    error: `Title must be at lease ${titleMinLength}  character(s).`,
+  }),
+  completed: z.boolean({ error: 'Completed must be boolean.' }),
+});
+
+export type AddTodoFormSchema = z.infer<typeof addTodoFormSchema>;
+
+export const defaultAddTodoFormValues: AddTodoFormSchema = {
+  title: '',
+  completed: false,
+};
+
+export const todoQueryKey = 'todos';
+
+export const getTodoQueryKey = () => {
+  return [todoQueryKey];
+};
+
+export const getTodoList = async (
+  params?: Partial<Pick<Todo, 'title' | 'completed'>>
+): Promise<Todo[]> => {
+  const url = new URL('http://localhost:3000/todos');
+
+  if (params?.title) {
+    url.searchParams.append('title:contains', params.title);
+  }
+
+  if (params?.completed !== undefined) {
+    url.searchParams.append('completed', params.completed.toString());
+  }
+
+  const response = await fetch(url.toString());
+  return await response.json();
+};
+
+export const addTodo = async (todo: Omit<Todo, 'id'>): Promise<Todo> => {
+  const response = await fetch('http://localhost:3000/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+  return await response.json();
+};
+
+export const updateTodo = async (todo: Todo): Promise<Todo> => {
+  const response = await fetch(`http://localhost:3000/todos/${todo.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+  return await response.json();
+};
+
+export const deleteTodo = async (todoId: Todo['id']): Promise<void> => {
+  const response = await fetch(`http://localhost:3000/todos/${todoId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return await response.json();
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const randomWidthPercentage = (
+  { min, max }: { min: number; max: number } = { min: 30, max: 100 }
+) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
